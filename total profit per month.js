4a5e6946211b97ffb6cd1662b23bbc4a8c8979c3
Andrew Crook <7helfs@gmail.com>
2022-11-13 13:11:59 +0200

    Fix error with wrong month

--- total profit per month.js
+++ total profit per month.js
@@ -1,6 +1,5 @@
 purchasesSales._arrayPurchases.filter(item => {
-    let month = 2678400000;
-    let date1 = new Date().getTime() - month;
+    let date1 = new Date('11.10.2022').getTime(); // month.day.year
     let date2 = new Date(item.update_time * 1000).getTime();
     let daysLag = Math.ceil(Math.abs(date2 - date1) / (1000 * 3600 * 24));
     return item.status === 'sold' && daysLag <= 31;
